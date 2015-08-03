cca5995a60b9bf513cfa731a147e857a9911ff2b
Joseph Le <josephle@me.com>
2015-08-02 22:27:45 -0700

    Added Guzzle

--- src/Typeform.php
+++ src/Typeform.php
@@ -2,6 +2,22 @@
 
 namespace Typeform;
 
+use GuzzleHttp;
+
 class Typeform {
 
+    /** @var  GuzzleHttp\Client */
+    protected $http;
+
+    /** @var  string */
+    protected $apiKey;
+
+    public function __construct($apiKey)
+    {
+        $this->http = new GuzzleHttp\Client([
+            // Base URI is used with relative requests
+            'base_uri' => 'https://api.typeform.com/v0/form/'
+        ]);
+        $this->apiKey = $apiKey;
+    }
 }
